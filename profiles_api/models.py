99209fd6fd9b8c7151c7b76091e60f87a4579fb2
kunle <balogunkunle@gmail.com>
2021-03-20 14:31:41 -0400

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,12 +13,12 @@ class UserProfileManager(BaseUserManager):
             raise valueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
-        return users
+        return user
 
     def create_superuser(self, email, name, password):
         """Create and save a ew superuser with given details"""
